819bf1eae6cae162c63ff2cd79e33976e9a03051
papercastl3 <jisung061444@gmail.com>
2023-01-23 23:27:00 +0900

    2023/01/23/23:26

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import receipt from './receipt.svg'
 import receipt_out from './receipt2.svg'
 import card from './card_barcode.svg'
-import qr_btn from './qr_point.svg'
+import qr_btn from './qr_btn_m.svg'
+import qr_btn_s from './qr_btn_s.svg'
 import ad1 from './ad1.svg'
 import ad2 from './ad2.svg'
 import eat_in from './eat_in.svg'
@@ -19,7 +20,7 @@ function Kiosk(){ //Kiosk component => 사용자 정의 함수= > 컴포넌트�
       </div>
       <div class="bottom">
           <div class="receipt_box">
-              <img src={receipt} alt="영수증 나오는 곳"/>
+              <img src={receipt} alt="영수증 나오는 곳" onChange=""/>
           </div>
           <div class="card_box">
               <img src={card} alt="카드 투입구"/>
@@ -58,8 +59,8 @@ function Screen(){ //초기화면 with ad
 function Screen2(){ //매장 or 화면 선택
   return(
     <div class="screen">
-      <img src={logo} width ="30px" height="30px" class="logo_img"></img>
-      <div>식사 방법을 {'\n'} 선택해 주세요</div>
+      <img src={logo} width ="30px" height="30px" style={{marginTop:"20px"}}></img>
+      <div style={{fontSize :"30px",fontWeight :"550"}}>식사 장소를 {'\n'} 선택해 주세요</div>
       <div class="btn_box">
         <div class="btn">
           <div class="text_area">매장</div>
@@ -70,21 +71,24 @@ function Screen2(){ //매장 or 화면 선택
           <img src={take_out} id="take_out"/> 
         </div>
       </div>
-      <div class ="sub_text">
-        언어
+      <div style={{fontSize:"10px",fontWeight :"500"}}>
+        언어 선택
       </div>
-
       <div class = "lang_select_boxes">
-        <div class ="lsb">
+        <div class ="lsb" id="Eng">
           English
         </div>
-        <div class ="lsb">
+        <div class ="lsb" id="Kr">
           한국어
         </div>
       </div>
-      <footer class ="footer">
-      
-      </footer>
+      <div class="footer_box">
+        <img src={qr_btn_s}/>
+        <div class="back_btns">
+          <div class="back_btn">처음으로</div>
+          <div class="back_btn">도움 기능 ...</div>
+        </div>
+      </div>
 
     </div>
   );
